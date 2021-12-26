c7ac02a47066215bb94c39aef27334ec12a4d31c
prajwal-naik <naikprajwal40@gmail.com>
2021-12-26 21:47:23 +0530

    responsiveness

--- components/CountriesDisplay.jsx
+++ components/CountriesDisplay.jsx
@@ -26,7 +26,7 @@ function CountriesDisplay({ countries }) {
 		<div className={`${themeStore === "dark"?"bg-[#202c37]":"bg-white"} px-[40px] sm:px-[150px] py-[40px] overflow-y-scroll scrollbar-hide`}>
 			
 			{/* {JSON.stringify(countries)} */}
-			<div className="grid auto-rows grid-cols-1 sm:grid-cols-4 gap-[40px] sm:gap-[80px] scrollbar-hide">
+			<div className="grid auto-rows grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-[40px] sm:gap-[80px] scrollbar-hide">
 				{
 					countries.map((country) => {
 						return(
